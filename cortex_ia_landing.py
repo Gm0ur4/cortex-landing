a788d328ef7001d3906206aac1f0e37ef82a1cbd
Gm0ur4 <97255018+Gm0ur4@users.noreply.github.com>
2026-01-22 15:45:38 -0300

    Update cortex_ia_landing.py

--- cortex_ia_landing.py
+++ cortex_ia_landing.py
@@ -543,7 +543,7 @@ st.markdown("""
 st.markdown("""
     <div class="container">
         <div class="hero">
-            <h1>🧠 A Cortex é a primeira plataforma desenvolvida para te ensinar comportamento em 21 dias</h1>
+            <h1>🧠 A Cortex é a primeira plataforma desenvolvida para te ensinar comportamento humano em 21 dias</h1>
             <p class="hero-subtitle">
                 Somos pioneiros a dar o fim em cursos chatos e PDFs intermináveis.<br>
                 <strong>São mais de 15.000 alunos absorvendo o conhecimento dos 22 maiores best-sellers do mundo</strong>
