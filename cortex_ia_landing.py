7f3b3d6d689b33fc951bed0fa53ea8490c4d38ea
inteligenciacortex <97255018+Gm0ur4@users.noreply.github.com>
2026-01-29 12:12:26 -0300

    Update cortex_ia_landing.py

--- cortex_ia_landing.py
+++ cortex_ia_landing.py
@@ -466,7 +466,7 @@ st.markdown("""
     
     /* DEMO SECTION */
     .demo-section {
-        background: #37D087;
+        background: #EFFBF6;
         padding: 60px 20px;
         margin: 40px 0;
         border-radius: 16px;
