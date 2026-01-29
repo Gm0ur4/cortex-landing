58361b463733dbda7ba350a5cb28bee6d0a052bf
inteligenciacortex <97255018+Gm0ur4@users.noreply.github.com>
2026-01-29 18:46:42 -0300

    Update cortex_ia_landing.py

--- cortex_ia_landing.py
+++ cortex_ia_landing.py
@@ -859,10 +859,10 @@ st.markdown("""
 st.markdown("""
     <div class="container">
         <div class="hero">
-            <h1>🧬 A Cortex é a primeira plataforma desenvolvida exclusivamente ao ensinamento do comportamento humano.</h1>
+            <h1>🧬 A Cortex é a primeira plataforma desenvolvida exclusivamente ao ensinamento do comportamento humano</h1>
             <p class="hero-subtitle">
                 <br>
-    <strong> Junte-se a mais de 15 mil pessoas que dominam as táticas das 22 maiores autoridades globais em psicologia e linguagem corporal.</strong>
+    <strong style="font-size: 1.3rem;">Junte-se a mais de 15 mil pessoas que dominam as táticas das 22 maiores autoridades globais em psicologia e linguagem corporal.</strong>
             </p>
             <a href="https://cortexcheckout.streamlit.app" target="_blank" class="hero-cta-link">
             <button class="hero-cta">
